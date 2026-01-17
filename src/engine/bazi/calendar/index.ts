b48d084d1343206a05e91d937fc2b3de52cd9b9e
send2medinfo <send2medinfo@gmail.com>
2026-01-17 11:20:28 +0800

    Fix core build: remove invalid barrel, exclude tests

--- src/engine/bazi/calendar/index.ts
+++ src/engine/bazi/calendar/index.ts
@@ -2,3 +2,4 @@ export * from "./sexagenary";
 export * from "./dayPillar";
 export * from "./hourPillar";
 export * from "./yearMonthPillar";
+export * from "./solar-term-adapter";
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
-export { SolarTermAdapterV1 } from "./bazi/calendar/solar-term-adapter";
-export { computeBaZiChart } from "./engine";
+export * from "./engine";
