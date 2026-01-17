fe0f16a298ed1933ef4b3704933956fd45fb5f61
send2medinfo <send2medinfo@gmail.com>
2026-01-17 11:46:17 +0800

    Expose SolarTermAdapterV1 via public calendar barrel

--- src/engine/bazi/calendar/index.ts
+++ src/engine/bazi/calendar/index.ts
@@ -2,4 +2,5 @@ export * from "./sexagenary";
 export * from "./dayPillar";
 export * from "./hourPillar";
 export * from "./yearMonthPillar";
+export * from "./adapters";
 export * from "./solar-term-adapter";
